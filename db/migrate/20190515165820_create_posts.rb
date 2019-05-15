3c689799a027c6a20d3e97aa3c99902a5d46e1b8
Aaron Lebel <alebel@Aarons-MacBook-Pro.local>
2019-05-15 13:24:28 -0400

    Initial commit

---
+++ db/migrate/20190515165820_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :posted
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
